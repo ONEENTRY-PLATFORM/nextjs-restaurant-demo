8bc34683a5cf25a062102a7360c79be1ca7788ab
Alexandr <kvasssukr.net@gmail.com>
2025-09-01 18:16:05 +0300

    0.0.03

---
+++ next.config.ts
@@ -0,0 +1,58 @@
+import type { NextConfig } from "next";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig: NextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
+    // optimizeCss: true,
+    optimizePackageImports: ['gsap', 'react-toastify'],
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+    deviceSizes: [640, 1920],
+    // imageSizes: [32, 64, 128, 256],
+    // qualities: [50, 75],
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oe-restaurants.oneentry.cloud',
+        port: '',
+        pathname: '/cloud-static/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/fonts/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
